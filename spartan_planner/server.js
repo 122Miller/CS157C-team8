5d75489b579bef33caff84a20f6f80cf23f3410c
122Miller <70303237+122Miller@users.noreply.github.com>
2021-04-18 21:26:18 -0700

    finished connection to MongoDB

--- spartan_planner/server.js
+++ spartan_planner/server.js
@@ -5,6 +5,11 @@ const cors = require('cors'); //helps avoid CORS issues
 const app = express();
 const port = process.env.PORT || 5000;
 
+const MongoClient = require('mongodb').MongoClient;
+const { connect } = require('mongodb');
+
+const mongoose = require('mongoose')
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,4 +25,37 @@ app.post('/api/world', (req, res) => {
   );
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+var uri = "mongodb+srv://spartan:sjsu@cluster0.pdfct.mongodb.net/test?retryWrites=true&w=majority";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+MongoClient.connect(uri, options).then(client => {
+  console.log("Database connection established");
+  const db = client.db('star-wars-quotes')
+  const quotesCollection = db.collection('quotes')
+
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => console.error(error))
+  })
+
+},
+err => {
+  {
+    console.log("Error connectiong database instance due to: ", err);
+  }
+});
+
